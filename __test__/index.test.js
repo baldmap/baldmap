4980ee9fd10c481d50aede8ccc1800eabad20496
Skyler Burger <skylerburger@gmail.com>
2019-03-04 09:28:41 -0800

    filled in readme

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,21 +1,16 @@
 'use strict';
 
-// const program = require('../index.js');
-
 const fs = require('fs');
 
 describe('bitmap transformations', () => {
     it('should process a valid bitmap file', () => {
         let file = __dirname + '/../assets/baldy.bmp';
-        console.log('Path: '+ file);
 
         function runTest(){
             let buffer = fs.readFileSync(file);
-            let output = buffer.toString('utf-8', 0, 2);
-
-            console.log(output);
-            return output;
+            return buffer.toString('utf-8', 0, 2);
         }
+
 
         expect(runTest()).toEqual('BM');
     });
